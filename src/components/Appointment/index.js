4aa5c5114178d4c4ba985aab81a920914b3763e0
Karl Jensen <karljensen@gmail.com>
2019-05-19 14:43:13 -0700

    Editing Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -15,6 +15,7 @@ import Confirm from "components/Appointment/Confirm";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const EDIT = "EDIT";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
@@ -49,12 +50,22 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
           onCancel={() => back()}
+          onSave={save}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel={back}
           onSave={save}
         />
       )}
