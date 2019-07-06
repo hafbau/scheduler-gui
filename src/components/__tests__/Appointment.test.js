a50720046bce8ac7400f9cf7558505dd13b3dbd0
Karl Jensen <karljensen@gmail.com>
2019-07-06 03:04:32 -0700

    Building Confidence

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,20 +5,20 @@ import { render } from "react-testing-library";
 import Appointment from "components/Appointment";
 
 describe("Appointment", () => {
+  const interview = {
+    student: "Lydia Miller-Jones",
+    interviewer: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  };
+
   it("renders without crashing", () => {
     render(<Appointment />);
   });
 
   it("renders without crashing when the mode is SHOW but the value of interview is null", () => {
-    const interview = {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    };
-
     const { getByText, rerender } = render(
       <Appointment interview={interview} />
     );
@@ -26,5 +26,29 @@ describe("Appointment", () => {
     expect(getByText("Lydia Miller-Jones")).toBeInTheDocument();
 
     rerender(<Appointment interview={null} />);
+  });
+
+  it("transitions correctly when the mode is SHOW but the value of interview is null", () => {
+    const { getByText, getByAltText, rerender } = render(
+      <Appointment interview={interview} />
+    );
+
+    expect(getByText("Lydia Miller-Jones")).toBeInTheDocument();
+
+    rerender(<Appointment interview={null} />);
+
+    expect(getByAltText("Add")).toBeInTheDocument();
+  });
+
+  it("transitions correctly when the mode is EMPTY but the value interview is not null", () => {
+    const { getByText, getByAltText, rerender } = render(
+      <Appointment interview={null} />
+    );
+
+    expect(getByAltText("Add")).toBeInTheDocument();
+
+    rerender(<Appointment interview={interview} />);
+
+    expect(getByText("Lydia Miller-Jones")).toBeInTheDocument();
   });
 });
