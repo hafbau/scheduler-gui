cf6585a59a3e91214ab8e75793419cce59aa3539
Karl Jensen <karljensen@gmail.com>
2019-08-12 23:02:31 -0700

    Remove redundant state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,18 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  
+
   function transition(mode, replace) {
-    setMode(mode);
-    setHistory(prev => 
-      (
-        replace
-        ? [...prev.slice(0, prev.length - 1), mode]
-        : [...prev, mode]
-      )
+    setHistory(prev =>
+      replace ? [...prev.slice(0, prev.length - 1), mode] : [...prev, mode]
     );
   }
 
   function back() {
     if (history.length < 2) return;
-    setMode(history[history.length - 2]);
-    setHistory(prev => ([...prev.slice(0, history.length - 1)]));
+    setHistory(prev => [...prev.slice(0, history.length - 1)]);
   }
 
-  return { mode, transition, back };
+  return { mode: history[history.length - 1], transition, back };
 }
